c930f80f83ff8a0dbf2693ab69e7b51238e4b663
Loc NguyenThien <loc.nguyent@saigontechnology.com>
2024-10-24 10:45:36 +0700

    Initial commit
    Generated by react-native@0.74.5

---
+++ ios/Pods/Target Support Files/Pods-FaceDetectCamera-FaceDetectCameraTests/Pods-FaceDetectCamera-FaceDetectCameraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceDetectCamera_FaceDetectCameraTests : NSObject
+@end
+@implementation PodsDummy_Pods_FaceDetectCamera_FaceDetectCameraTests
+@end
